18ee8a494b95f104b6552e3740e8f2c42c136311
Sonic2607 <40140332+Sonic2607@users.noreply.github.com>
2020-02-20 13:07:41 +0300

    Update Free_1.js

--- Free_1.js
+++ Free_1.js
@@ -31,7 +31,7 @@
   GM_setValue('minDelayBetweenOrdersSec', [65, 600])
   GM_setValue('reloadOnConnectionReset', true)
   GM_setValue('delayLevelToMinOrder', 1.5)
-  GM_setValue('delayLevelToStop', 3.5)
+  GM_setValue('delayLevelToStop', 3.1)
   GM_setValue('orderMinDelay', 1)
 
   GM_setValue('maxBalance', 501)
