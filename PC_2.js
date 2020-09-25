804734d20b7f6231487787d8a178be0c9b9cf9fb
Sonic2607 <40140332+Sonic2607@users.noreply.github.com>
2020-09-25 15:08:53 +0300

    Update PC_2.js

--- PC_2.js
+++ PC_2.js
@@ -34,7 +34,7 @@
   GM_setValue('diffSafe_ZECUSD', [-100, 100])
   GM_setValue('diffSafe_DASHUSD', [-100, 100])
  
-  GM_setValue('maxBalance', 253)
+  GM_setValue('maxBalance', 305)
   GM_setValue('minBalance', 65)
   GM_setValue('stopLossPercent', 11)
   GM_setValue('maxOpenTrades', 1)
