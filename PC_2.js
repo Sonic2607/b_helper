532069680f674a94a6987c298554e8587ce84c2f
Sonic2607 <40140332+Sonic2607@users.noreply.github.com>
2020-10-23 09:53:22 +0300

    Update PC_2.js

--- PC_2.js
+++ PC_2.js
@@ -5,12 +5,12 @@
   GM_setValue('percentThreshold_ZECUSD', 80)
   GM_setValue('percentThreshold_DASHUSD', 80)
  
-  GM_setValue('dynLevelCoef_BTCUSD', 1.8)
-  GM_setValue('dynLevelCoef_ETHUSD', 1.9)
-  GM_setValue('dynLevelCoef_XRPUSD', 1.89)
-  GM_setValue('dynLevelCoef_ETCUSD', 1.89)
-  GM_setValue('dynLevelCoef_ZECUSD', 1.96)
-  GM_setValue('dynLevelCoef_DASHUSD', 1.96)
+  GM_setValue('dynLevelCoef_BTCUSD', [1.8, 1.8])
+  GM_setValue('dynLevelCoef_ETHUSD', [1.8, 1.8])
+  GM_setValue('dynLevelCoef_XRPUSD', [1.8, 1.8])
+  GM_setValue('dynLevelCoef_ETCUSD', [1.8, 1.8])
+  GM_setValue('dynLevelCoef_ZECUSD', [1.8, 1.8])
+  GM_setValue('dynLevelCoef_DASHUSD', [1.8, 1.8])
  
   GM_setValue('dynLevelSafe_BTCUSD', 59)
   GM_setValue('dynLevelSafe_ETHUSD', 59)
